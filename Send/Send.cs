76dfe8033b737d1ba2c4edf2583a8f9ad508949e
Ruben Corona Jacobo <ruben.cj@gmail.com>
2022-12-30 15:44:55 -0600

    Hello RabbitMQ

---
+++ Send/Send.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var facory = new ConnectionFactory() { HostName = "localhost" };
+
+        using(var connection = facory.CreateConnection())
+        {
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete:false, arguments: null);
+                string message = "Hola Ruben!";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+                Console.Write($"[x] Send {message}");
+
+            }
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadLine();
+    }
+}
